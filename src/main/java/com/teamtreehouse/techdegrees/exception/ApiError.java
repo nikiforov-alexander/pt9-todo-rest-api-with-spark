7bda86dc266d10327904561460f23c2bdd592f92
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-08-14 20:58:10 +0200

    added ApiError just like in REST course. Will be used in Exception Handling

---
+++ src/main/java/com/teamtreehouse/techdegrees/exception/ApiError.java
@@ -0,0 +1,13 @@
+package com.teamtreehouse.techdegrees.exception;
+
+public class ApiError extends RuntimeException{
+    private final int status;
+    public ApiError(int status, String msg) {
+        super(msg);
+        this.status = status;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
