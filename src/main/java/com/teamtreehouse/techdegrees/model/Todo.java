3d5a9e50257d7004a695104470716132ff1c4e1a
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-08-14 20:36:51 +0200

    added `toString()` just in case

--- src/main/java/com/teamtreehouse/techdegrees/model/Todo.java
+++ src/main/java/com/teamtreehouse/techdegrees/model/Todo.java
@@ -49,4 +49,12 @@ public class Todo {
         result = 31 * result + (name != null ? name.hashCode() : 0);
         return result;
     }
+
+    @Override
+    public String toString() {
+        return "Todo { " +
+                "id = " + id +
+                ", name = '" + name + '\'' +
+                " }";
+    }
 }
