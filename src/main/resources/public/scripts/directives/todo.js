940f980ad4fdc631e80c343819c816dc8d6db07b
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-11-22 14:38:28 +0100

    Add restrict to element opt in todo.js directive
    now I know the difference, and alos this way Intellijdea is happy, however without this one, we can use both ...

--- src/main/resources/public/scripts/directives/todo.js
+++ src/main/resources/public/scripts/directives/todo.js
@@ -3,6 +3,8 @@
 angular.module('todoListApp')
 .directive('todo', function(){
   return {
+    // restrict : 'A', // 'Attribute name' so that <div todo></div> works
+    restrict : 'E', // 'Element name' so that <todo></todo> works
     templateUrl: 'templates/todo.html',
     replace: true,
     controller: 'todoCtrl'
